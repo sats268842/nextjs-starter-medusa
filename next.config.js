4914f915b73f79a30376e61bb49feabba8ea2de3
Santhosh Thomas <santhoshthomas015@gmail.com>
2023-01-25 15:18:49 +0530

    s3

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
-    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost"],
+    domains: ["test-vajraah-admin-site.ap-south-1.amazonaws.com", "localhost"],
   },
   webpack: (config, options) => {
     if (options.isServer) {
