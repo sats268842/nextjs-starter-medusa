3c0caee1432ca20148bce5bfa0fb1cba69f0089a
Santhosh Thomas <santhosh.thomas@tsworkz.com>
2023-01-26 13:30:15 +0530

    added

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
         Order, chill, and enjoy—we&apos;ll deliver your favourites to you.
          
         </p>
-        <UnderlineLink className="text-black" href="/store" >Explore Menu</UnderlineLink>
+        <UnderlineLink  href="/store" >Explore Menu</UnderlineLink>
       </div>
       <Image
         src="https://images.unsplash.com/photo-1579027989536-b7b1f875659b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format"
