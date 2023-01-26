6555f8e202f3b273fea8693ed63e41955c2ee976
Santhosh Thomas <santhosh.thomas@tsworkz.com>
2023-01-26 13:18:18 +0530

    added

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -5,21 +5,21 @@ const Hero = () => {
   return (
     <div className="h-[90vh] w-full relative">
       <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
-        <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
-          Summer styles are finally here
+        <h1 className="text-6xl font-bold mb-4 drop-shadow-md shadow-black ">
+        What do you like <br></br> to eat right now?
         </h1>
-        <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
-          This year, our new summer collection will shelter you from the harsh
-          elements of a world that doesn&apos;t care if you live or die.
+        <p className="text-lg max-w-[32rem] mb-6 drop-shadow-md shadow-black text-gray-300">
+        Order, chill, and enjoy—we&apos;ll deliver your favourites to you.
+         
         </p>
-        <UnderlineLink href="/store">Explore products</UnderlineLink>
+        <UnderlineLink className="text-black" href="/store" >Explore Menu</UnderlineLink>
       </div>
       <Image
-        src="/hero.jpg"
+        src="https://images.unsplash.com/photo-1579027989536-b7b1f875659b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format"
         layout="fill"
         loading="eager"
         priority={true}
-        quality={90}
+        quality={100}
         objectFit="cover"
         alt="Photo by @thevoncomplex https://unsplash.com/@thevoncomplex"
         className="absolute inset-0"
--- src/modules/layout/components/footer-cta/index.tsx
+++ src/modules/layout/components/footer-cta/index.tsx
@@ -6,15 +6,15 @@ const FooterCTA = () => {
     <div className="bg-amber-100 w-full">
       <div className="content-container flex flex-col-reverse gap-y-8 small:flex-row small:items-center justify-between py-16 relative">
         <div>
-          <h3 className="text-2xl-semi">Shop the latest styles</h3>
+          <h3 className="text-2xl-semi">Hungry? <br></br> Great Food Delivered at your door step</h3>
           <div className="mt-6">
-            <UnderlineLink href="/store">Explore products</UnderlineLink>
+            <UnderlineLink href="/store">Explore menu</UnderlineLink>
           </div>
         </div>
 
         <div className="relative w-full aspect-square small:w-[35%] small:aspect-[28/36]">
           <Image
-            src="/cta_three.jpg"
+            src="https://images.unsplash.com/photo-1530554764233-e79e16c91d08?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format"
             alt=""
             layout="fill"
             objectFit="cover"
